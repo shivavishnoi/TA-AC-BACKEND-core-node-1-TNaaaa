e3c94022e3997d11eda7c0d253c0e3417d802b4a
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-10-03 08:22:40 +0530

    node require

--- block-BNaaad/index.js
+++ block-BNaaad/index.js
@@ -1,4 +1,12 @@
-console.log(`welcome to node`);
-for (let i = 0; i < 11; i++) {
-  console.log(i);
-}
+// console.log(`welcome to node`);
+// for (let i = 0; i < 11; i++) {
+//   console.log(i);
+// }
+
+var path = require('path');
+var os = require('os');
+var path = require('path');
+
+var filepath = path.join(__dirname, 'index.md');
+var freeMem = os.freemem();
+console.log(filepath, freeMem, os.cpus().length);
